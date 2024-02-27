22c643dbc985b38b3242063774f16f1bdedd80fa
LAB2-PC27 <LAB2-PC27@DESKTOP-FRQB679>
2024-02-26 21:48:06 -0300

    segundo commit

---
+++ Aula_4/src/model/ContaPoupança.java
@@ -0,0 +1,39 @@
+package model;
+
+public class ContaPoupança extends Conta{
+	private double rendimento;
+	ContaPoupança (String email) {
+		super(email);
+		this.rendimento = rendimento;
+	
+	}
+	public String Saque (double valorSaque) {
+		if(valorSaque <= saldo) {
+			saldo =- valorSaque;
+			return (
+				"Valor Sacado: " + valorSaque
+				+"\nValor Saldo Após o Saque: " 
+			    + saldo
+			    
+				);
+			
+					
+		} else {
+			return "Saldo Insuficiente";
+		}
+	}
+	
+	public String Deposito(double valorDeposito) {
+		saldo += (valorDeposito + (valorDeposito*rendimento));
+		return ("Valor Sacado: " + valorDeposito
+				+"\nValor Saldo Após o Saque: "
+				+ saldo);
+	}
+	
+	public double getSaldo() {
+		
+		return saldo;
+	}
+
+}
+
